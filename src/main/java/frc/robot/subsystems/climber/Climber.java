4c657aebcc0abecaab07a7f7747a7af4732a5ed1
Vlad Roiban <119555710+Vladdapenn@users.noreply.github.com>
2025-01-06 15:28:05 -0500

    logger change

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -34,6 +34,6 @@ public class Climber extends SubsystemBase {
   public void periodic() {
     // This method will be called once per scheduler run
     io.updateInputs(inputs);
-    Logger.getInstance().processInputs("Climber", inputs);
+    Logger.processInputs("Climber", inputs);
   }
 }
