371e1b2cc278ed0e74abc6042105c41a18291730
Vlad Roiban <119555710+Vladdapenn@users.noreply.github.com>
2025-01-06 16:38:55 -0500

    stash 2

--- src/main/java/frc/robot/subsystems/vision/AprilTagVisionIOPhotonvision.java
+++ src/main/java/frc/robot/subsystems/vision/AprilTagVisionIOPhotonvision.java
@@ -5,6 +5,7 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.DoubleArraySubscriber;
 import edu.wpi.first.networktables.DoubleSubscriber;
